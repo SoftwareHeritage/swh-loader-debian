43545c3160b45d05ed5d1d24bd243370c6c23ab1
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-10-01 14:46:35 +0200

    Add necessary files to package the module

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def parse_requirements():
+    requirements = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
+
+    return requirements
+
+
+setup(
+    name='swh.loader.debian',
+    description='Software Heritage Debian loader',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/DLDDEB/',
+    packages=['swh.loader.debian', 'swh.loader.debian.tests'],
+    scripts=['bin/swh-loader-debian'],
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)
